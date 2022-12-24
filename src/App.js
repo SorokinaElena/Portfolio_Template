fca795dbe98974f4b997adde85dc39a69dcf3b4c
eleno <e.lenoksls@gmail.com>
2022-12-25 00:00:07 +0100

    block home added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AboutMe from './components/AboutMe';
+import Home from './components/Home';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div> 
       <Nav />
+      <Home />
       <AboutMe />
     </div>
   );
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import home_photo from './images/home_photo.png'
+import s from './index.module.css'
+
+export default function Home() {
+  return (
+    <div className={['wrapper', s.home].join(' ')}>
+        <div className={s.home_content}>
+            <h1>Denis <br /> Novik</h1>
+            <p>UX | UI designer <br /> 24 years old, Minsk</p>
+            <h2> <span className={s.ru_text_color}>RU</span> | ENG</h2>
+        </div>
+        <img src={home_photo} alt="photo" />
+    </div>
+  )
+}
